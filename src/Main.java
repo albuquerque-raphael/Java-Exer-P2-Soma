998cf68210e909cbd973f22f0d6f8e2716eadf9f
Raphael Albuquerque <albuquerquec.raphael@gmail.com>
2024-01-25 15:26:51 -0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,13 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
 
-        Scanner scanner = new Scanner(System.in);
+        Calculadora c = new Calculadora();
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Digite o primeiro número: ");
-        double numero1 = scanner.nextDouble();
-
-        System.out.println("Digite o segundo número: ");
-        double numero2 = scanner.nextDouble();
-
-        double resultado = numero1 + numero2;
-
-        System.out.println("A soma dos números é igual a " + resultado);
-
-        scanner.close();
+        System.out.println("Digite um número inteiro: ");
+        c.n1 = sc.nextInt();
+        System.out.println("Digite outro número inteiro: ");
+        c.n2 = sc.nextInt();
+        c.Somar();
     }
 }
